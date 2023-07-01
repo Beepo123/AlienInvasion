4e380ab29a6793cb0c944a96799520b3677c7e19
Samuel Gipit <samuelgipit@gmail.com>
2023-07-01 21:17:25 +0800

    fixed ship with background

--- ship.py
+++ ship.py
@@ -1,15 +1,17 @@
 import pygame
 
+
 class Ship:
     """A class to manage a ship"""
+
     def __init__(self, ai_game):
         """Initialize the ship and set its starting position"""
         self.screen = ai_game.screen
         self.screen_rect = ai_game.screen.get_rect()
 
         # Load the ship image and get its rect
-        DEFAULT_IMAGE_SIZE = (70,50)
-        self.image = pygame.image.load('images/ship.png')
+        DEFAULT_IMAGE_SIZE = (70, 50)
+        self.image = pygame.image.load("images/ship.png")
         self.image = pygame.transform.scale(self.image, DEFAULT_IMAGE_SIZE)
         self.rect = self.image.get_rect()
 
