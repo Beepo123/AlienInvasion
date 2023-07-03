3a3954da9ef407d9e46803d8bcd2d8734e4a5923
Samuel Gipit <samuelgipit@gmail.com>
2023-07-03 18:48:11 +0800

    added continous movement

--- ship.py
+++ ship.py
@@ -18,6 +18,23 @@ class Ship:
         # Start ship at the bottom center of the screen
         self.rect.midbottom = self.screen_rect.midbottom
 
+        # Movement flag
+        self.moving_right = False
+        self.moving_left = False
+        self.moving_up = False
+        self.moving_down = False
+        
+    def update(self):
+        """Update ship position based on moving flag"""
+        if self.moving_right:
+            self.rect.x += 1
+        elif self.moving_left:
+            self.rect.x -= 1
+        elif self.moving_up:
+            self.rect.y -= 1
+        elif self.moving_down:
+            self.rect.y += 1
+
     def blitme(self):
         """Draw the ship at its current location"""
         self.screen.blit(self.image, self.rect)
